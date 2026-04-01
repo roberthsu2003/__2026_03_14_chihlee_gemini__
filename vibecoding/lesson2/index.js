e6f0feaf5acaee7f9d30b19a920a8fe495f71467
roberthsu2003 <roberthsu2003@gmail.com>
2026-04-01 15:29:41 +0800

    modify

--- vibecoding/lesson2/index.js
+++ vibecoding/lesson2/index.js
@@ -1,4 +1,10 @@
-console.log("Hello from Node.js!");
-console.log("你好，歡迎使用 Node.js！");
-console.log("這是一個繁體中文訊息");
-console.log("程式執行成功！");
+import axios from "axios";
+
+async function main() {
+  const res = await axios.get("https://httpbin.org/get", { timeout: 8000 });
+  console.log("axios 請求成功，HTTP 狀態:", res.status);
+}
+
+main().catch((err) => {
+  console.error("請確認已執行 npm install axios\n", err.message);
+});
